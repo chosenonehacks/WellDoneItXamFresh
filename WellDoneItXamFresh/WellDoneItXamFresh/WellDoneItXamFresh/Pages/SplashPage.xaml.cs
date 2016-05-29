cabbfc092eb9658abd125647e42e2453fb9e4fef
hal9k2 <hal9k2@hotmail.com>
2016-05-29 22:18:22 +0100

    Change to Android tabbed navigation

--- WellDoneItXamFresh/WellDoneItXamFresh/WellDoneItXamFresh/Pages/SplashPage.xaml.cs
+++ WellDoneItXamFresh/WellDoneItXamFresh/WellDoneItXamFresh/Pages/SplashPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WellDoneItXamFresh.Helpers;
 using WellDoneItXamFresh.PageModels;
 using Xamarin.Forms;
+using FreshMvvm;
 
 namespace WellDoneItXamFresh.Pages
 {
@@ -26,19 +27,25 @@ namespace WellDoneItXamFresh.Pages
                 // delay for a few seconds on the splash screen
                 await Task.Delay(3000);
                 
-                var masterDetailNav = new ThemedMasterDetailNavigationContainer(); // FreshMasterDetailNavigationContainer();
-
-                masterDetailNav.Init("Menu", "menu.png");
-                masterDetailNav.AddPageWithIcon<InboxPageModel>("Inbox", "inboxm.png");
-                masterDetailNav.AddPageWithIcon<TodayPageModel>("Today", "todaym.png");
-                masterDetailNav.AddPageWithIcon<ContextListPageModel>("Contexts", "contextm.png");
+//                var masterDetailNav = new ThemedMasterDetailNavigationContainer(); // FreshMasterDetailNavigationContainer();
+//
+//                masterDetailNav.Init("Menu", "menu.png");
+//                masterDetailNav.AddPageWithIcon<InboxPageModel>("Inbox", "inboxm.png");
+//                masterDetailNav.AddPageWithIcon<TodayPageModel>("Today", "todaym.png");
+//                masterDetailNav.AddPageWithIcon<ContextListPageModel>("Contexts", "contextm.png");
 				//temp switch settings off as not much there anyway
 //                masterDetailNav.AddPageWithIcon<SettingsPageModel>("Settings", "settingsm.png");
+				var mainPage = new FreshTabbedNavigationContainer();
+			
+				mainPage.AddTab<InboxPageModel>("Inbox", "inboxm.png");
+				mainPage.AddTab<TodayPageModel>("Today", "eventm.png");
+				mainPage.AddTab<ContextListPageModel>("Contexts", "contextm.png");
+
                 
 
                 // on the main UI thread, set the MainPage to the navPage
                 Device.BeginInvokeOnMainThread(() => {
-                    Application.Current.MainPage = masterDetailNav;
+					Application.Current.MainPage = mainPage;
                 });
             });
 
